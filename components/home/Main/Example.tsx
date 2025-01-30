d81634d7dcceade92d6b0653e9678c2fa184f0c2
MigicalLeo <leo92816leo92816@gmail.com>
2025-01-30 18:20:06 +0800

    added welcome page

---
+++ components/home/Main/Example.tsx
@@ -0,0 +1,45 @@
+import { MdOutlineTipsAndUpdates } from "react-icons/md";
+import examples from "@/data/examples.json";
+import Button from "@/components/common/Button";
+import { useMemo, useState } from "react";
+export default function Exaple() {
+  const [showFull, setShowFull] = useState(false);
+  const list = useMemo(() => {
+    if (showFull) {
+      return examples;
+    } else {
+      return examples.slice(0, 15);
+    }
+  }, [showFull]);
+  return (
+    <div className="flex flex-col items-center">
+      <div className="mt-20 mb-4 text-4xl">
+        <MdOutlineTipsAndUpdates />
+      </div>
+
+      <ul className="flex justify-center flex-wrap gap-3.5">
+        {list.map((example) => (
+          <li key={example.act} className="flex flex-col items-center">
+            <Button>{example.act}</Button>
+          </li>
+        ))}
+      </ul>
+      {!showFull && (
+        <>
+          <p className="p-2">...</p>
+          <div className="flex w-full items-center space-x-2">
+            <hr className="flex-1 border-t border-dotted border-gray-200 dark:border-gray-600" />
+            <Button
+              onClick={() => {
+                setShowFull(true);
+              }}
+            >
+              Show All
+            </Button>
+            <hr className="flex-1 border-t border-dotted border-gray-200 dark:border-gray-600" />
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
--- components/home/Main/Index.tsx
+++ components/home/Main/Index.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Menu from "./Menu";
+import Welcome from "./Wlecome";
 
 export default function Main() {
   return (
-    <main className="flex-1 bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100">
+    <main className="overflow-y-auto flex-1 bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100">
       <Menu />
+      <Welcome />
     </main>
   );
 }
